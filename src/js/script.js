31ad7b8a03a2f492793e0a4fdf4e7c8426123024
gustavo962 <guff009ff@gmail.com>
2026-02-25 11:07:04 -0300

    Delete src directory

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.getElementById('btn');
-
-  btn.addEventListener('click', () => {
-    alert('Funcionando com Gulp 🚀');
-  });
-});
